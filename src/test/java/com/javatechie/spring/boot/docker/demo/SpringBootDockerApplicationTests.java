2343e235b5449dbd2e07c1bed78db4370f014407
aumohite <ashishmohite@gmail.com>
2025-05-16 19:36:02 +0530

    second commit

---
+++ src/test/java/com/javatechie/spring/boot/docker/demo/SpringBootDockerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.javatechie.spring.boot.docker.demo;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootDockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
